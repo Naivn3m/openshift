3ccafa6f5b75a085c1e9fc6911aa52fe273d83d7
Navin Das <navindude5@gmail.com>
2018-03-27 16:45:59 +0530

    Add files via upload

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+@SpringBootApplication
+public class DemoApplication {
+
+	
+	@RequestMapping("/greeting")
+	@ResponseBody
+	String home(){
+		return "Greetings from Zeppelin";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
